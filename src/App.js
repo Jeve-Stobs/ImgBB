441962bbced5f2d1ef3120cbc9d84ec64547e64e
Samuel Casey <76224164+Jeve-Stobs@users.noreply.github.com>
2021-03-09 22:35:32 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './App.css';
+import Upload from "./uploader/Upload";
+
+function App() {
+  return (
+    <div>
+      <div className="content">
+        <Upload />
+      </div>
+    </div>
+  );
+}
+
+export default App;
